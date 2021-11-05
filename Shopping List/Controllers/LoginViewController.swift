b42b68e1d00db3d1b201e681fa76c8c78983443d
LukichevSergey <Lukichev_s@mail.ru>
2021-11-05 12:29:01 +0700

    Функционал переноса контента над клавиатурой

--- Shopping List/Controllers/LoginViewController.swift
+++ Shopping List/Controllers/LoginViewController.swift
@@ -16,7 +16,26 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Когда клавиатура появляется на экране
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        
+        //Когда клавиатура убирается с экрана
+        NotificationCenter.default.addObserver(self, selector: #selector(keyBoardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        
+        
+    }
+    
+    @objc func keyBoardDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        let keyBoardFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height + keyBoardFrameSize.height)
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyBoardFrameSize.height, right: 0)
+    }
+    
+    @objc func keyBoardDidHide() {
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height)
     }
 
     @IBAction func loginTapped(_ sender: UIButton) {
